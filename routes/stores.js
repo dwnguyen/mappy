e784197c4c1b2375f5495a5b758712c92dfda31c
David Nguyen <dwnguyen@ucsd.edu>
2020-03-11 03:16:16 -0700

    Added nearby stops functionality

--- routes/stores.js
+++ routes/stores.js
@@ -4,17 +4,26 @@ exports.view = function (req, res) {
   var endNodes = req.params.endNodes;
   var storeType = req.params.storeType;
   var version = req.params.version;
+  var showNearby = "no";
+  var startGate = startNodes.split("+")[0]
+  var endNodesArray = endNodes.split("+")
+  var endGate = endNodesArray[endNodesArray.length-1]
   var stores = [];
   for (var i = 0; i < nodes.length; i++) {
     var node = nodes[i]
     if (node.category === storeType) stores.push(node);
   }
+  if(startGate == "Gate49" && endGate == "Gate39") showNearby="yes"
+  else showNearby ="no"
   stores.stringify = JSON.stringify(stores);
+
+  console.log(showNearby)
   res.render("stores", {
     'stores': stores,
     'storeType': storeType,
     'startNodes': startNodes,
     'endNodes': endNodes,
-    'version': version
+    'version': version,
+    'showNearby': showNearby
   });
 };
